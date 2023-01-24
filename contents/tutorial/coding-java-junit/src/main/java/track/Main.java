155995d683962795f70a9aeb5416d93125fbaab7
Syogo-Suganoya <registration@ssuganoya.com>
2023-01-24 13:38:55 +0900

    create 作成完了

---
+++ contents/tutorial/coding-java-junit/src/main/java/track/Main.java
@@ -0,0 +1,9 @@
+package track;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        System.out.print(App.add(x, y));
+    }
+}
---
+++ contents/tutorial/coding-java-junit/src/test/java/track/Util.java
@@ -0,0 +1,29 @@
+package track;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class Util {
+    private static long TIMEOUT = 5_000;
+
+    public static void runApp(String[] args) {
+        AppThread th = new AppThread(args);
+        try {
+            th.start();
+            th.join(TIMEOUT);
+        } catch (InterruptedException e) {
+            fail("Timeout " + TIMEOUT + "ms");
+        }
+    }
+
+    private static class AppThread extends Thread {
+        private final String[] args;
+
+        AppThread(String[] args) {
+            this.args = args;
+        }
+
+        public void run() {
+            Main.main(args);
+        }
+    }
+}
