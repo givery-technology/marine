2a7ad5b0a5e849b26a4414a262c612bfd0e9b56e
Syogo-Suganoya <registration@ssuganoya.com>
2023-03-04 11:55:33 +0900

    fix レビュー修正

--- contents/tutorial/coding-java-junit/src/test/java/AppTest.java
+++ contents/tutorial/coding-java-junit/src/test/java/AppTest.java
@@ -1,50 +1,34 @@
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.ByteArrayOutputStream;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 @Timeout(5)
 public class AppTest {
   @Test
-  public void test_2_3() throws IOException {
-    __testOutput("src/test/resources/in/public/01.in", "src/test/resources/out/public/01.out");
+  public void test_2_3() {
+    int[] input = {2, 3};
+    __testOutput(input, 5);
   }
+
   @Test
-  public void test_15_8() throws IOException {
-    __testOutput("src/test/resources/in/public/02.in", "src/test/resources/out/public/02.out");
+  public void test_15_8() {
+    int[] input = {15, 8};
+    __testOutput(input, 23);
   }
+
   @Test
-  public void test_65_94() throws IOException {
-    __testOutput("src/test/resources/in/public/03.in", "src/test/resources/out/public/03.out");
+  public void test_65_94() {
+    int[] input = {65, 94};
+    __testOutput(input, 159);
   }
 
-  private void __testOutput(String input, String expected) throws IOException {
-    PrintStream origin = System.out;
-    try {
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-      System.setOut(new PrintStream(bos, true, "utf-8"));
-      String[] args = Files.readString(Paths.get(input), StandardCharsets.UTF_8).trim().split(" ");
-      int x = Integer.parseInt(args[0]);
-      int y = Integer.parseInt(args[1]);
-      Util.runApp(x, y);
-      String result = new String(bos.toByteArray(), "utf-8").trim();
+  private void __testOutput(int[] input, int expected) {
+    int x = input[0];
+    int y = input[1];
+    int result = Util.runApp(x, y);
 
-      List<String> expectedContent = Files.readAllLines(Paths.get(expected));
-      String output = String.join("\n",expectedContent).trim();
-
-      assertThat(result, is(output));
-    } finally {
-      System.setOut(origin);
-    }
+    assertThat(result, is(expected));
   }
 }
--- contents/tutorial/coding-java-junit/src/test/java/Util.java
+++ contents/tutorial/coding-java-junit/src/test/java/Util.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class Util {
   private static long TIMEOUT = 5_000;
 
-  public static void runApp(int x, int y) {
+  public static int runApp(int x, int y) {
     AppThread th = new AppThread(x, y);
     try {
       th.start();
@@ -11,19 +11,25 @@ public class Util {
     } catch (InterruptedException e) {
       fail("Timeout " + TIMEOUT + "ms");
     }
+    return th.getResult();
   }
 
   private static class AppThread extends Thread {
     private final int x;
     private final int y;
+    private int result = -1;
 
     AppThread(int x, int y) {
       this.x = x;
       this.y = y;
     }
 
+    int getResult() {
+      return this.result;
+    }
+
     public void run() {
-      System.out.print(App.add(x, y));
+      this.result = App.add(x, y);
     }
   }
 }
