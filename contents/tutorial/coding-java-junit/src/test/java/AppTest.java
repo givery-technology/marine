5f580d2b5c02471bf6870b4c6e2d3cae5eb39923
snakazawa <nakazawa.tms0314@gmail.com>
2023-03-17 22:35:57 +0900

    use gradle in tutorial/coding-java-junit

--- contents/tutorial/coding-java-junit/src/test/java/AppTest.java
+++ contents/tutorial/coding-java-junit/src/test/java/AppTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
@@ -7,18 +8,21 @@ import static org.hamcrest.CoreMatchers.is;
 @Timeout(5)
 public class AppTest {
   @Test
+  @DisplayName("[基本実装] 入力が 2 と 3 のとき、期待された出力結果が得られる")
   public void test_2_3() {
     int[] input = {2, 3};
     __testOutput(input, 5);
   }
 
   @Test
+  @DisplayName("[基本実装] 入力が 15 と 8 のとき、期待された出力結果が得られる")
   public void test_15_8() {
     int[] input = {15, 8};
     __testOutput(input, 23);
   }
 
   @Test
+  @DisplayName("[基本実装] 入力が 65 と 94 のとき、期待された出力結果が得られる")
   public void test_65_94() {
     int[] input = {65, 94};
     __testOutput(input, 159);
