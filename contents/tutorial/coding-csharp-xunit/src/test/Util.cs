bdab6c8c2e75ce912aa44ecb425ad5931ab0087b
Syogo-Suganoya <registration@ssuganoya.com>
2023-03-26 20:27:32 +0900

    fix インデント統一

--- contents/tutorial/coding-csharp-xunit/src/test/Util.cs
+++ contents/tutorial/coding-csharp-xunit/src/test/Util.cs
@@ -3,43 +3,43 @@ using System.Threading;
 
 public class Util
 {
-	private static int TIMEOUT = 5000;
-	public static int RunApp(int x, int y)
-	{
-		AppThread appThread = new AppThread(x, y);
-		Thread th = new Thread(new ThreadStart(appThread.Run));
-		try
-		{
-			th.Start();
-			th.Join(TIMEOUT);
-		}
-		catch (ThreadInterruptedException)
+    private static int TIMEOUT = 5000;
+    public static int RunApp(int x, int y)
+    {
+        AppThread appThread = new AppThread(x, y);
+        Thread th = new Thread(new ThreadStart(appThread.Run));
+        try
         {
-			Console.WriteLine("Timeout " + TIMEOUT + "ms");
+            th.Start();
+            th.Join(TIMEOUT);
         }
-		return appThread.GetResult();
-	}
+        catch (ThreadInterruptedException)
+        {
+            Console.WriteLine("Timeout " + TIMEOUT + "ms");
+        }
+        return appThread.GetResult();
+    }
 
-	private class AppThread
-	{
-		private readonly int x;
-		private readonly int y;
-		private int result = -1;
+    private class AppThread
+    {
+        private readonly int x;
+        private readonly int y;
+        private int result = -1;
 
-		public AppThread(int x, int y)
-		{
-			this.x = x;
-			this.y = y;
-		}
+        public AppThread(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
 
-		public int GetResult()
-		{
-			return this.result;
-		}
+        public int GetResult()
+        {
+            return this.result;
+        }
 
-		public void Run()
-		{
-			this.result = App.Add(x, y);
-		}
-	}
+        public void Run()
+        {
+            this.result = App.Add(x, y);
+        }
+    }
 }
