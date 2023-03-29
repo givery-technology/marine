576b74f6639f89816eaef63fb1378006fec97189
Syogo-Suganoya <registration@ssuganoya.com>
2023-03-29 11:38:13 +0900

    fix metatestのinstall

--- contents/tutorial/coding-csharp-xunit/scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-# BEGIN_CHALLENGE
-# Imageにmetatestがないため、インストール
-# グローバルインストールするため、package.jsonは使用していません。
-# END_CHALLENGE
-npm install -g givery-technology/track-metatest-tools;
--- contents/tutorial/coding-csharp-xunit/scripts/test_run.sh
+++ contents/tutorial/coding-csharp-xunit/scripts/test_run.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
 cd /root/src/src/test
 dotnet test --logger:xunit;
-
-source /root/src/scripts/install.sh  # metatest実行と同じファイル内でinstallしないと 「metatest: not found」 が出力される
-metatest do echo then expand xunit /root/src/src/test/TestResults/TestResults.xml;
+$(npm bin)/metatest do echo then expand xunit /root/src/src/test/TestResults/TestResults.xml;
