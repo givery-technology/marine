f5fdf463511058f9dadf03a868869fd22893c8b0
Syogo-Suganoya <registration@ssuganoya.com>
2023-03-31 20:20:02 +0900

    add DisplayName

--- contents/tutorial/coding-csharp-xunit/src/test/AppTest.cs
+++ contents/tutorial/coding-csharp-xunit/src/test/AppTest.cs
@@ -2,21 +2,21 @@ using Xunit;
 
 public class AppTest
 {
-    [Fact]
+    [Fact(DisplayName = "[基本実装] 入力が 2 と 3 のとき、期待された出力結果が得られる")]
     public void test_2_3()
     {
         int[] input = new int[2] {2, 3};
         __testOutput(input, 5);
     }
 
-    [Fact]
+    [Fact(DisplayName = "[基本実装] 入力が 15 と 8 のとき、期待された出力結果が得られる")]
     public void test_15_8()
     {
         int[] input = new int[2] {15, 8};
         __testOutput(input, 23);
     }
 
-    [Fact]
+    [Fact(DisplayName = "[基本実装] 入力が 65 と 94 のとき、期待された出力結果が得られる")]
     public void test_65_94()
     {
         int[] input = new int[2] {65, 94};
